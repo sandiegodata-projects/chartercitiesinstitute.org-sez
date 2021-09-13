dfbc48cfa27ebcce5865609c0b22ffd8c1517937
Eric Busboom <eric@civicknowledge.com>
2021-09-13 12:45:36 -0700

    .

--- notebooks/ericbusboom/lib.py
+++ notebooks/ericbusboom/lib.py
@@ -2,6 +2,7 @@ import rasterio
 from joblib import Parallel, delayed
 from tqdm.notebook import tqdm
 import numpy as np 
+import pandas as pd
 
 # Load a year of rasters, masked and cropped to a geometry
 # See the rasterio documentation for more examples: 
@@ -74,3 +75,67 @@ def build_ring_sums(ntl_p, sez_df, radius=10_000):
     rows = [ (e[0],e[1], np.nansum(e[2])) for e in patches]
 
     return sez_df, patches, pd.DataFrame(rows, columns=['year','sez_id','pix_sum'])
+
+
+def split_series(df, col=None):
+    
+    col = col or 'mean_light'
+    
+    bf = df[df.year<=df.op_year].sort_values('year').reset_index(drop=True)
+    af = df[df.year>=df.op_year].sort_values('year').reset_index(drop=True)
+
+    bf[col] = bf[col].fillna(method='bfill').fillna(method='ffill')
+    af[col] = af[col].fillna(method='bfill').fillna(method='ffill')
+    
+    return bf, af
+
+
+def exp_line_fit(x,y=None, col='mean_light'):
+    
+    from numpy.linalg import LinAlgError 
+    
+    if y is None:
+        df = x
+        x = df['year']
+        y = df[col]
+    else:
+        df = None
+    
+    y = np.log(y)
+    
+    try:
+        fit = np.polyfit(x,y,1)
+    
+    except LinAlgError:
+        # Didn't converge
+        return [np.nan,np.nan]
+    except TypeError:
+        # Maybe that x or y is empty
+        return [np.nan,np.nan]
+    except ValueError:
+        # Something Else, maybe that x or y is empty
+        return [np.nan,np.nan]
+    
+        
+    beta, alpha = np.exp(fit)
+    
+    return beta-1, alpha
+
+
+
+def compute_yoy(df, col=None):
+    
+    bf, af = split_series(df, col)
+    
+    def _f(t):
+        ml = t.mean_light
+        return (ml.iloc[-1]-ml.iloc[0])/len(ml)
+        
+    return pd.Series([_f(bf), _f(af)])
+
+def compute_slopes(df, col=None):
+
+    bf, af = split_series(df, col)
+    
+
+    return pd.Series([exp_line_fit(bf)[0], exp_line_fit(af)[0]])
